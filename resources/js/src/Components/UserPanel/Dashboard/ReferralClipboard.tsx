b96a23b3ad336a138ae4c13ee4bcc1ceca70e492
mostafizrr2 <dev.mostafiz@gmail.com>
2022-03-14 02:51:27 +0600

    hle

--- resources/js/src/Components/UserPanel/Dashboard/ReferralClipboard.tsx
+++ resources/js/src/Components/UserPanel/Dashboard/ReferralClipboard.tsx
@@ -5,13 +5,13 @@ import { HiClipboardCheck, HiClipboardCopy } from 'react-icons/hi'
 
 export default function ReferralClipboard() {
 
-    const [value, setValue] = useState('https://salient-demo.com/?ref_id=443535')
+    const [value] = useState('https://salient-demo.com/?ref_id=443535')
     const { hasCopied, onCopy } = useClipboard(value)
 
     return (
         <Flex mb={2}>
           <Input value={value} isReadOnly placeholder='Welcome' />
-          <Button onClick={onCopy} ml={2} bg={hasCopied && 'green.200'}>
+          <Button onClick={onCopy} ml={2} bg={hasCopied ? 'green.200' : 'whiteAlpha.300'}>
             {hasCopied ? <Icon as={HiClipboardCheck} h={6} w={6} color='green.500' />  : <Icon as={HiClipboardCopy} h={6} w={6} />}
           </Button>
         </Flex>
