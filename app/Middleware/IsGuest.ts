fcb0efadbd59aeaa63563d0e1bc27d9f66923397
mostafizrr2 <dev.mostafiz@gmail.com>
2022-03-26 19:33:23 +0600

    sd

--- app/Middleware/IsGuest.ts
+++ app/Middleware/IsGuest.ts
@@ -2,7 +2,7 @@ import { AuthenticationException } from '@adonisjs/auth/build/standalone'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class IsGuest {
-  public async handle({ auth, response }: HttpContextContract, next: () => Promise<void>) {
+  public async handle({ auth }: HttpContextContract, next: () => Promise<void>) {
     // code for middleware goes here. ABOVE THE NEXT CALL
     console.log('Auth check: ', await auth.check())
     
