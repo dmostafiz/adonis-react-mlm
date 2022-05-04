83ea1edd4e4461584d6c1cb9429ada152c753803
mostafizrr2 <dev.mostafiz@gmail.com>
2022-05-05 00:49:54 +0600

    dfg

--- resources/js/src/Components/UserPanel/Dashboard/AccountInfoCards.tsx
+++ resources/js/src/Components/UserPanel/Dashboard/AccountInfoCards.tsx
@@ -1,4 +1,4 @@
-import { Flex, useColorModeValue, chakra, Text, Stack, Grid, GridItem, Button, Link } from '@chakra-ui/react'
+import { Flex, useColorModeValue, chakra, Text, Stack, Grid, GridItem, Link } from '@chakra-ui/react'
 import React from 'react'
 
 export default function AccountInfoCards({ childUsers, clicks }) {
